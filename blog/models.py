24d726afb247e0c1ff34dbe6706f45fea87426c5
Omkar <omkar.achrekar007@gmail.com>
2021-04-25 02:03:36 +0530

    Functioning project complete

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     dody = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.dody[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,8 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# where to look for static file
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio/static/'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
